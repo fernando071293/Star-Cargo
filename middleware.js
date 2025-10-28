8b4b668d8cd9787cb524554878da8a5e6c391360
EngelAlexey <alexhmanzanares@gmail.com>
2025-10-28 11:26:07 -0600

    Commit

---
+++ middleware.js
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.rewrite(new URL('/es/home/', request.url));
+  }
+  return NextResponse.next();
+}
--- middlewareddd.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(request) {
-  console.log('request.nextUrl.pathname', request.nextUrl.pathname);
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.rewrite(new URL('/es/home', request.url))
-  }
-}
